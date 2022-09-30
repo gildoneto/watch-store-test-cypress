3537422776416671c3dd0405a7911f1092078330
Gildo Neto <gildo.neto@telus.com>
2022-09-30 14:22:27 -0300

    TDD - adiciona método addToCart

--- components/ProductCard.unit.spec.js
+++ components/ProductCard.unit.spec.js
@@ -2,8 +2,26 @@ import { mount } from '@vue/test-utils';
 import ProductCard from '@/components/ProductCard';
 import { makeServer } from '@/miragejs/server';
 
+let server;
+
+const mountProductCard = () => {
+  const product = server.create('product', {
+    title: 'Relógio Bonito',
+    price: '22.00',
+    image:
+      'https://images.unsplash.com/photo-1532667449560-72a95c8d381b?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
+  });
+  return {
+    wrapper: mount(ProductCard, {
+      propsData: {
+        product,
+      },
+    }),
+    product,
+  };
+};
+
 describe('ProductCard - unit', () => {
-  let server;
   beforeEach(() => {
     server = makeServer({ environment: 'test' });
   });
@@ -12,36 +30,32 @@ describe('ProductCard - unit', () => {
   });
 
   it('should match snapshot', () => {
-    const wrapper = mount(ProductCard, {
-      propsData: {
-        product: server.create('product', {
-          title: 'Relógio Bonito',
-          price: '22.00',
-          image:
-            'https://images.unsplash.com/photo-1532667449560-72a95c8d381b?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
-        }),
-      },
-    });
+    const { wrapper } = mountProductCard();
 
     expect(wrapper.element).toMatchSnapshot();
   });
 
   it('should mount the component', () => {
-    const wrapper = mount(ProductCard, {
-      propsData: {
-        product: server.create('product', {
-          title: 'Relógio Bonito',
-          price: '22.00',
-        }),
-      },
-    });
-
-    // console.log(wrapper.html());
-    // console.log(wrapper.classes());
-    // console.log(wrapper.element);
-    // console.log(wrapper.exists());
+    const { wrapper } = mountProductCard();
     expect(wrapper.vm).toBeDefined();
     expect(wrapper.text()).toContain('Relógio Bonito');
     expect(wrapper.text()).toContain('$22.00');
   });
+  it('should emit the event addToCart with product object when button gets clicked', async () => {
+    const { wrapper, product } = mountProductCard();
+
+    await wrapper.find('button').trigger('click');
+
+    expect(wrapper.emitted().addToCart).toBeTruthy();
+    expect(wrapper.emitted().addToCart.length).toBe(1);
+    expect(wrapper.emitted().addToCart[0]).toEqual([{ product }]);
+  });
 });
+
+/**
+ * Métodos/propriedades interessantes:
+ * wrapper.html()
+ * wrapper.classes()
+ * wrapper.element
+ * wrapper.exists()
+ */
