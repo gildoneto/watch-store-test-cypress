b21fe0b49468e0c513a7c20b5e007b3df46037d2
Gildo Neto <gildo.neto@telus.com>
2022-10-04 11:52:58 -0300

    implementa método de busca

--- pages/ProductList.integration.spec.js
+++ pages/ProductList.integration.spec.js
@@ -69,4 +69,37 @@ describe('ProductList - integration', () => {
 
     expect(wrapper.text()).toContain('Problemas ao carregar a lista');
   });
+
+  it('should filter the product list when a search is performed', async () => {
+    // ARRANGE
+    const products = [
+      ...server.createList('product', 10),
+      server.create('product', {
+        title: 'Meu relógio amado',
+      }),
+      server.create('product', {
+        title: 'Meu outro relógio estimado',
+      }),
+    ];
+
+    axios.get.mockReturnValue(Promise.resolve({ data: { products } }));
+
+    const wrapper = mount(ProductList, {
+      mocks: {
+        $axios: axios,
+      },
+    });
+
+    await Vue.nextTick();
+
+    // ACT
+    const search = wrapper.findComponent(Search);
+    search.find('input[type="search"]').setValue('relógio');
+    await search.find('form').trigger('submit');
+
+    // ASSERT
+    const cards = wrapper.findAllComponents(ProductCard);
+    expect(wrapper.vm.searchTerm).toEqual('relógio');
+    expect(cards).toHaveLength(2);
+  });
 });
