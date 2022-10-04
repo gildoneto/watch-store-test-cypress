42433563006883d11bb323444493ee991eb0fd00
Gildo Neto <gildo.neto@telus.com>
2022-10-04 11:59:50 -0300

    unfilter test case

--- pages/ProductList.integration.spec.js
+++ pages/ProductList.integration.spec.js
@@ -102,4 +102,35 @@ describe('ProductList - integration', () => {
     expect(wrapper.vm.searchTerm).toEqual('relógio');
     expect(cards).toHaveLength(2);
   });
+  it('should unfilter the product list when the input search is cleared', async () => {
+    // ARRANGE
+    const products = [
+      ...server.createList('product', 10),
+      server.create('product', {
+        title: 'Meu relógio amado',
+      }),
+    ];
+
+    axios.get.mockReturnValue(Promise.resolve({ data: { products } }));
+
+    const wrapper = mount(ProductList, {
+      mocks: {
+        $axios: axios,
+      },
+    });
+
+    await Vue.nextTick();
+
+    // ACT
+    const search = wrapper.findComponent(Search);
+    search.find('input[type="search"]').setValue('relógio');
+    await search.find('form').trigger('submit');
+    search.find('input[type="search"]').setValue('');
+    await search.find('form').trigger('submit');
+
+    // ASSERT
+    const cards = wrapper.findAllComponents(ProductCard);
+    expect(wrapper.vm.searchTerm).toEqual('');
+    expect(cards).toHaveLength(11);
+  });
 });
