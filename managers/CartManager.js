4e87328de1f24faab109ab9aa5158d6259c096c9
Gildo Neto <gildo.neto@telus.com>
2022-10-12 11:18:59 -0300

    implementa testes CartManager

--- managers/CartManager.js
+++ managers/CartManager.js
@@ -12,9 +12,56 @@ export class CartManager {
     this.state = Vue.observable(initialState);
   }
 
+  getState() {
+    return this.state;
+  }
+
   open() {
     this.state.open = true;
 
-    return this.state;
+    return this.getState();
+  }
+
+  close() {
+    this.state.open = false;
+
+    return this.getState();
+  }
+
+  productIsInTheCart(product) {
+    return !!this.state.items.find(({ id }) => id === product.id);
+  }
+
+  hasProducts() {
+    return this.state.items.length > 0;
+  }
+
+  addProduct(product) {
+    if (!this.productIsInTheCart(product)) {
+      this.state.items.push(product);
+    }
+
+    return this.getState();
+  }
+
+  removeProduct(productId) {
+    this.state.items = [
+      ...this.state.items.filter((product) => product.id !== productId),
+    ];
+
+    return this.getState();
+  }
+
+  clearProducts() {
+    this.state.items = [];
+
+    return this.getState();
+  }
+
+  clearCart() {
+    this.clearProducts();
+    this.close();
+
+    return this.getState();
   }
 }
--- managers/CartManager.unit.spec.js
+++ managers/CartManager.unit.spec.js
@@ -1,23 +1,97 @@
 import { CartManager } from '@/managers/CartManager';
+import { makeServer } from '@/miragejs/server';
+
 describe('CartManager', () => {
+  let server;
+  let manager;
+
+  beforeEach(() => {
+    manager = new CartManager();
+    server = makeServer({ environment: 'test' });
+  });
+  afterEach(() => {
+    server.shutdown();
+  });
+
+  it('should return the state', () => {
+    const product = server.create('product');
+    manager.open();
+    manager.addProduct(product);
+    const state = manager.getState();
+
+    expect(state).toEqual({
+      items: [product],
+      open: true,
+    });
+  });
+
   it('should set cart to open', () => {
-    const manager = new CartManager();
     const state = manager.open();
 
     expect(state.open).toBe(true);
   });
 
-  it.todo('should set cart to closed');
+  it('should set cart to closed', () => {
+    const state = manager.close();
 
-  it.todo('should set add product to the cart only once');
+    expect(state.open).toBe(false);
+  });
 
-  it.todo('should remove product from the cart');
+  it('should set add product to the cart only once', () => {
+    const product = server.create('product');
+    manager.addProduct(product);
+    const state = manager.addProduct(product);
 
-  it.todo('should clear products');
+    expect(state.items).toHaveLength(1);
+  });
 
-  it.todo('should clear cart');
+  it('should remove product from the cart', () => {
+    const product = server.create('product');
+    const state = manager.removeProduct(product.id);
 
-  it.todo('should return true if cart is not empty');
+    expect(state.items).toHaveLength(0);
+  });
 
-  it.todo('should return true if product is already in the cart');
+  it('should clear products', () => {
+    const product1 = server.create('product');
+    const product2 = server.create('product');
+
+    manager.addProduct(product1);
+    manager.addProduct(product2);
+
+    const state = manager.clearProducts();
+
+    expect(state.items).toHaveLength(0);
+  });
+
+  it('should clear cart', () => {
+    const product1 = server.create('product');
+    const product2 = server.create('product');
+
+    manager.addProduct(product1);
+    manager.addProduct(product2);
+    manager.open();
+
+    const state = manager.clearCart();
+
+    expect(state.items).toHaveLength(0);
+    expect(state.open).toBe(false);
+  });
+
+  it('should return true if cart is not empty', () => {
+    const product1 = server.create('product');
+    const product2 = server.create('product');
+
+    manager.addProduct(product1);
+    manager.addProduct(product2);
+
+    expect(manager.hasProducts()).toBe(true);
+  });
+
+  it('should return true if product is already in the cart', () => {
+    const product = server.create('product');
+    manager.addProduct(product);
+
+    expect(manager.productIsInTheCart(product)).toBe(true);
+  });
 });
