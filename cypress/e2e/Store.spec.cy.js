940d24888f1f20bbba23ec910b17bfd2a5cd0e87
Gildo Neto <gildo.neto@telus.com>
2022-10-16 10:06:57 -0300

    fix: instala cypress 9.2.1 e configura miragejs

--- cypress/e2e/Store.spec.cy.js
+++
@@ -1,8 +0,0 @@
-context('Store', () => {
-  it('should display the store', () => {
-    cy.visit('http://localhost:3000');
-
-    cy.get('body').contains('Brand');
-    cy.get('body').contains('Wrist Watch');
-  });
-});
