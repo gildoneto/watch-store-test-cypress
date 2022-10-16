a1a1040c5c12816a4e85bd0f44970fe73ef87ea7
Gildo Neto <gildo.neto@telus.com>
2022-10-16 12:25:57 -0300

    feat: finaliza e2e search test

--- cypress/e2e/Store.spec.js
+++ cypress/e2e/Store.spec.js
@@ -4,6 +4,7 @@ import { makeServer } from '../../miragejs/server';
 
 context('Store', () => {
   let server;
+  const PRODUCT_TITLE = 'Relógio bonito';
 
   beforeEach(() => {
     server = makeServer({ environment: 'test' });
@@ -30,17 +31,26 @@ context('Store', () => {
         .should('have.value', TYPE_TEXT);
     });
 
-    it.only('should type in the search field', () => {
-      const PRODUCT_TITLE = 'Relógio bonito';
+    it('should return 1 product when "Relógio bonito" is used as search term', () => {
       server.create('product', {
         title: PRODUCT_TITLE,
       });
       server.createList('product', 10);
+
       cy.visit('http://localhost:3000');
       cy.get('input[type="search"]').type(PRODUCT_TITLE);
       cy.get('[data-testid="search-form"]').submit();
-
       cy.get('[data-testid="product-card"]').should('have.length', 1);
+    });
+
+    it('should not retorn any product', () => {
+      server.createList('product', 10);
+
+      cy.visit('http://localhost:3000');
+      cy.get('input[type="search"]').type(PRODUCT_TITLE);
+      cy.get('[data-testid="search-form"]').submit();
+      cy.get('[data-testid="product-card"]').should('have.length', 0);
+      cy.get('body').contains('0 Products');
     });
   });
 });
