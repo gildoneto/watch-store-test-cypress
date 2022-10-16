29b6bdf825d20e5c26feef273e4cd229c9e6d72b
Gildo Neto <gildo.neto@telus.com>
2022-10-16 12:19:27 -0300

    feat: teste search

--- cypress/e2e/Store.spec.js
+++ cypress/e2e/Store.spec.js
@@ -19,4 +19,28 @@ context('Store', () => {
     cy.get('body').contains('Brand');
     cy.get('body').contains('Wrist Watch');
   });
+
+  context('Store > Search for products', () => {
+    it('should type in the search field', () => {
+      const TYPE_TEXT = 'Some text here';
+      cy.visit('http://localhost:3000');
+
+      cy.get('input[type="search"]')
+        .type(TYPE_TEXT)
+        .should('have.value', TYPE_TEXT);
+    });
+
+    it.only('should type in the search field', () => {
+      const PRODUCT_TITLE = 'Relógio bonito';
+      server.create('product', {
+        title: PRODUCT_TITLE,
+      });
+      server.createList('product', 10);
+      cy.visit('http://localhost:3000');
+      cy.get('input[type="search"]').type(PRODUCT_TITLE);
+      cy.get('[data-testid="search-form"]').submit();
+
+      cy.get('[data-testid="product-card"]').should('have.length', 1);
+    });
+  });
 });
